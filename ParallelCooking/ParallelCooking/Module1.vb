d8d21973fadad1bae744a1535dde9181b3dd1f1b
Jeroen Moonen <jeroen.moonen@telenet.be>
2014-05-30 10:39:46 +0200

    main

--- ParallelCooking/ParallelCooking/Module1.vb
+++ ParallelCooking/ParallelCooking/Module1.vb
@@ -1,7 +1,13 @@
 ﻿Module Module1
 
     Sub Main()
+        Utils.Calibrate()
+        'Utils.SetNumberOfProcessors(1);
+        Dim sw As Stopwatch = Stopwatch.StartNew()
+        'GoulashRecipe.MakeGoulash()
+        Console.WriteLine("We're done in {0} minutes.", sw.ElapsedMilliseconds / 60)
 
+        Console.ReadKey()
     End Sub
 
 End Module
