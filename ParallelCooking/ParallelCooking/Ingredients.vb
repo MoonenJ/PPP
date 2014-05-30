ca86504eda5a7355984ef44033956d82a2ef0299
Jeroen Moonen <jeroen.moonen@telenet.be>
2014-05-30 10:23:21 +0200

    VBeginner Ingedients

---
+++ ParallelCooking/ParallelCooking/Ingredients.vb
@@ -0,0 +1,16 @@
+﻿Class Ingredient
+End Class
+Class Onion
+    Inherits Ingredient
+End Class
+Class Potato
+    Inherits Ingredient
+End Class
+Class Meat
+    Inherits Ingredient
+End Class
+Class Ingredients
+    Public Shared Meat As Meat() = New Meat(399) {}
+    Public Shared Potatoes As Potato() = New Potato(9) {}
+    Public Shared Onions As Onion() = New Onion(2) {}
+End Class
